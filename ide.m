e7203d7e25f697e3249d70411063f003075e3eb6
Omar Rizwan <omar.rizwan@gmail.com>
2019-07-15 21:37:18 -0700

    Get text edit event.

--- ide.m
+++ ide.m
@@ -1,12 +1,39 @@
 #import <Cocoa/Cocoa.h>
-int main ()
-{
-    @autoreleasepool{
+
+void recompile() {
+    
+}
+
+@interface IdeTextStorageDelegate: NSObject<NSTextStorageDelegate>
+
+@end
+
+@implementation IdeTextStorageDelegate
+- (void)textStorage:(NSTextStorage *)textStorage 
+  didProcessEditing:(NSTextStorageEditActions)editedMask 
+              range:(NSRange)editedRange 
+     changeInLength:(NSInteger)delta {
+    NSLog(@"hi");    
+}
+@end
+
+int main() {
+    @autoreleasepool {
         [NSApplication sharedApplication];
         [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
         id applicationName = [[NSProcessInfo processInfo] processName];
-        id window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 120, 120)
-                                                styleMask:NSTitledWindowMask backing:NSBackingStoreBuffered defer:NO];
+
+        NSRect contentFrame = NSMakeRect(0, 0, 600, 600);
+        id window = [[NSWindow alloc] initWithContentRect:contentFrame
+                                                styleMask:NSTitledWindowMask
+                                                  backing:NSBackingStoreBuffered defer:NO];
+
+        id textView = [[NSTextView alloc] initWithFrame:contentFrame];
+        id textStorageDelegate = [[IdeTextStorageDelegate alloc] init];
+        [[textView textStorage] setDelegate:textStorageDelegate];
+        [window setContentView:textView];
+        [window makeFirstResponder:textView];
+
         [window cascadeTopLeftFromPoint:NSMakePoint(20,20)];
         [window setTitle: applicationName];
         [window makeKeyAndOrderFront:nil];
