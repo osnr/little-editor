d59caa5bc779eb3018ba7e96682b0fa0ecc4d3b3
Omar Rizwan <omar.rizwan@gmail.com>
2019-07-15 23:02:39 -0700

    Keyboard shortcuts.

--- ide.m
+++ ide.m
@@ -17,9 +17,52 @@ void recompile() {
 }
 @end
 
+@interface IdeApplication: NSApplication
+@end
+
+@implementation IdeApplication
+- (void) sendEvent:(NSEvent *)event {
+    if ([event type] == NSKeyDown) {
+        if (([event modifierFlags] & NSDeviceIndependentModifierFlagsMask) == NSCommandKeyMask) {
+            if ([[event charactersIgnoringModifiers] isEqualToString:@"x"]) {
+                if ([self sendAction:@selector(cut:) to:nil from:self])
+                    return;
+            }
+            else if ([[event charactersIgnoringModifiers] isEqualToString:@"c"]) {
+                if ([self sendAction:@selector(copy:) to:nil from:self])
+                    return;
+            }
+            else if ([[event charactersIgnoringModifiers] isEqualToString:@"v"]) {
+                if ([self sendAction:@selector(paste:) to:nil from:self])
+                    return;
+            }
+            else if ([[event charactersIgnoringModifiers] isEqualToString:@"z"]) {
+                if ([self sendAction:@selector(undo:) to:nil from:self])
+                    return;
+            }
+            else if ([[event charactersIgnoringModifiers] isEqualToString:@"a"]) {
+                if ([self sendAction:@selector(selectAll:) to:nil from:self])
+                    return;
+            }
+        }
+        else if (([event modifierFlags] & NSDeviceIndependentModifierFlagsMask) == (NSCommandKeyMask | NSShiftKeyMask)) {
+            if ([[event charactersIgnoringModifiers] isEqualToString:@"Z"]) {
+                if ([self sendAction:@selector(redo:) to:nil from:self])
+                    return;
+            }
+        }
+    }
+    [super sendEvent:event];
+}
+
+// Blank Selectors to silence Xcode warnings: 'Undeclared selector undo:/redo:'
+- (IBAction)undo:(id)sender {}
+- (IBAction)redo:(id)sender {}
+@end
+
 int main() {
     @autoreleasepool {
-        [NSApplication sharedApplication];
+        [IdeApplication sharedApplication];
         [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
         id applicationName = [[NSProcessInfo processInfo] processName];
 
