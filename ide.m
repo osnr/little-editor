b3132fb24691bf72bd75aff98ff8c44a78cf9d40
Omar Rizwan <omar.rizwan@gmail.com>
2019-07-18 03:23:00 -0700

    Highlight test. Can add/remove attributes.

--- ide.m
+++ ide.m
@@ -14,16 +14,63 @@ static void interpreterInit() {
     luaL_openlibs(L);
 }
 
-static int l_setDefaultFont(lua_State *L) {
+static int l_setdefaultfont(lua_State *L) {
     NSString *fontName = [NSString stringWithUTF8String:luaL_checkstring(L, 1)];
     int fontSize = luaL_checknumber(L, 2);
     [textView setFont:[NSFont fontWithName:fontName size:fontSize]];
     return 0;
 }
 
+static int l_makerange(lua_State *L) {
+    NSRange *range = lua_newuserdata(L, sizeof(NSRange));
+    *range = NSMakeRange(luaL_checknumber(L, 1) - 1, luaL_checknumber(L, 2));
+    return 1;
+}
+static int l_makecolor(lua_State *L) {
+    NSString *colorName = [NSString stringWithUTF8String:luaL_checkstring(L, 1)];
+    NSColor *color = [NSColor valueForKey:colorName];
+    lua_pushlightuserdata(L, (__bridge void *) color);
+    return 1;
+}
+static int l_addattribute(lua_State *L) {
+    NSAttributedStringKey name = [NSString stringWithUTF8String:lua_tostring(L, 1)];
+    id value = (__bridge id) lua_touserdata(L, 2);
+    NSRange *range = lua_touserdata(L, 3);
+    @try {
+        // FIXME: propagate errors better.
+        [[textView textStorage] addAttribute:name
+                                       value:value
+                                       range:*range];
+    } @catch (NSException *e) {
+        NSLog(@"%@", e);
+    }
+    return 0;
+}
+static int l_removeattribute(lua_State *L) {
+    NSAttributedStringKey name = [NSString stringWithUTF8String:lua_tostring(L, 1)];
+    NSRange *range = lua_touserdata(L, 2);
+    @try {
+        // FIXME: propagate errors better.
+        [[textView textStorage] removeAttribute:name
+                                          range:*range];
+    } @catch (NSException *e) {
+        NSLog(@"%@", e);
+    }
+    return 0;
+}
+
 static void interpreterRun() {
-    lua_pushcfunction(L, l_setDefaultFont);
+    lua_pushcfunction(L, l_setdefaultfont);
     lua_setglobal(L, "setdefaultfont");
+
+    lua_pushcfunction(L, l_makerange);
+    lua_setglobal(L, "makerange");
+    lua_pushcfunction(L, l_makecolor);
+    lua_setglobal(L, "makecolor");
+    lua_pushcfunction(L, l_removeattribute);
+    lua_setglobal(L, "removeattribute");
+    lua_pushcfunction(L, l_addattribute);
+    lua_setglobal(L, "addattribute");
 
     luaL_dofile(L, "language.lua");
 }
@@ -34,6 +81,11 @@ static void interpreterHook(const char *s) {
         lua_pushstring(L, s);
         lua_pcall(L, 1, 0, 0);
     }
+}
+
+static void interpreterWatchCallback() {
+    interpreterRun();
+    interpreterHook([[[textView textStorage] string] UTF8String]);
 }
 
 @interface IdeTextStorageDelegate: NSObject<NSTextStorageDelegate>
@@ -145,7 +197,7 @@ int main() {
         // Set up a file-watcher that will rerun when the Lua script
         // changes. (It runs on the single main CF run loop on the
         // NSApp, I think, so no threading issues!)
-        watch("language.lua", &interpreterRun);
+        watch("language.lua", &interpreterWatchCallback);
 
         [NSApp activateIgnoringOtherApps:YES];
         [NSApp run];
--- language.lua
+++ language.lua
@@ -3,5 +3,8 @@ print("Running language.lua!")
 setdefaultfont("Menlo", 15)
 
 function hook(s)
+   print(math.floor(#s /2))
+   removeattribute("NSBackgroundColor", makerange(1, #s))
+   addattribute("NSBackgroundColor", makecolor("orangeColor"), makerange(1, math.floor(#s/2)))
    print("new string: ["..s.."]")
 end
